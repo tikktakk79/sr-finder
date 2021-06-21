12af3e249880221bfe619de2da87d56cbacb1210
Mårten Sjöberg <marten.sjoberg@gmail.com>
2021-06-21 11:26:00 +0200

    improved mobile

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="bg-warmgray-50 min-h-screen w-full">
     <NavBar :name="$route.name" />
     <div class="flex justify-center">
-      <div class="flex-grow left-gradient min-h-screen"></div>
+      <div class="hidden sm:block flex-grow left-gradient min-h-screen"></div>
       <div class="w-full sm:max-w-2xl flex-grow-max">
         <router-view
           class="bg-white w-full min-h-screen px-4 py-12"
@@ -28,7 +28,7 @@
           </div>
         </div>
       </div>
-      <div class="flex-grow right-gradient min-h-screen"></div>
+      <div class="hidden sm:block flex-grow right-gradient min-h-screen"></div>
     </div>
   </div>
 </template>
